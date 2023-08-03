1b30ec8545c5607dc2afad6023f3e726819a1bd9
Zachary Sunberg <zachary.sunberg@colorado.edu>
2023-08-03 14:59:26 -0600

    add more flexible termination

--- src/solver.jl
+++ src/solver.jl
@@ -1,15 +1,32 @@
-Base.@kwdef struct SARSOPSolver{LOW,UP} <: Solver
-    epsilon::Float64    = 0.5
-    precision::Float64  = 1e-3
-    kappa::Float64      = 0.5
-    delta::Float64      = 1e-1
-    max_time::Float64   = 1.0
-    max_steps::Int      = typemax(Int)
-    verbose::Bool       = true
-    init_lower::LOW     = BlindLowerBound(bel_res = 1e-2)
-    init_upper::UP      = FastInformedBound(bel_res=1e-2)
-    prunethresh::Float64= 0.10
-    ρ::Float64          = 3.0
+Base.@kwdef struct SARSOPSolver{LOW,UP,TERM} <: Solver
+    epsilon::Float64     = 0.5
+    kappa::Float64       = 0.5
+    delta::Float64       = 1e-1
+    max_time::Float64    = 1.0
+    max_steps::Int       = typemax(Int)
+    verbose::Bool        = true
+    init_lower::LOW      = BlindLowerBound(bel_res = 1e-2)
+    init_upper::UP       = FastInformedBound(bel_res=1e-2)
+    prunethresh::Float64 = 0.10
+    terminate::TERM      = RootDifference(0.001)
+    # ρ::Float64          = 3.0
+end
+
+struct RootDifference <: Function
+    precision::Float64
+end
+
+(rd::RootDifference)(tree) = root_diff(tree) <= rd.precision
+
+# below doesn't need to go in this package
+struct CGCPRootDifference <: Function
+    rho::Float64
+    precision::Float64
+end
+
+function (c::CGCPRootDifference)(tree)
+    ga = 10^(log10(max(abs(tree.V_upper[1]),abs(tree.V_lower[1])))-c.ρ)
+    return root_diff(tree) <= ga
 end
 
 function solve_info(solver::SARSOPSolver, pomdp::POMDP)
@@ -17,8 +34,7 @@ function solve_info(solver::SARSOPSolver, pomdp::POMDP)
 
     t0 = time()
     iter = 0
-    ga = 10^(log10(max(abs(tree.V_upper[1]),abs(tree.V_lower[1])))-solver.ρ)
-    while time()-t0 < solver.max_time && root_diff(tree) > ga #root_diff(tree) > solver.precision
+    while time()-t0 < solver.max_time && !solver.terminate(tree)
         sample!(solver, tree)
         backup!(tree)
         prune!(solver, tree)
